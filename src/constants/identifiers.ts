5a4556e764cc1a8db6ad3e23a0d42fe1c003dc69
Kamil Solecki <kamilczak020@gmail.com>
2019-12-03 16:49:44 +0100

    Added identifier for matcher config

--- src/constants/identifiers.ts
+++ src/constants/identifiers.ts
@@ -1,6 +1,7 @@
 export const SERVICE_IDENTIFIER = {
   IBot: Symbol.for('Bot'),
   ILogger: Symbol.for('Logger'),
+  IMatcherConfig: Symbol.for('IMatcherConfig'),
   DiscordClient: Symbol.for('DiscordClient'),
   DiscordToken: Symbol.for('DiscordToken'),
 };
