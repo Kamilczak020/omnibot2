49a8f565e233664707122accb25f60e09203a567
Kamilczak020 <kamilczak020@gmail.com>
2019-12-03 23:50:31 +0100

    Fixed error handling, added regexp type

--- src/config/loader.ts
+++ src/config/loader.ts
@@ -1,20 +1,26 @@
 import * as t from 'io-ts';
 import { isRight } from 'fp-ts/lib/Either';
-import { safeLoad } from 'js-yaml';
-import { readFileSync } from 'fs';
-import { ConfigurationError } from 'src/error';
+import { readFileSync, existsSync } from 'fs';
+import { ConfigurationError, NotFoundError } from 'src/error';
+import { DEFAULT_SAFE_SCHEMA, safeLoad, Schema } from 'js-yaml';
+import { regexpType } from './regexp';
+
+export const CONFIG_SCHEMA = Schema.create([DEFAULT_SAFE_SCHEMA], [
+  regexpType,
+]);
 
 export function loadConfig<T>(codec: t.Type<T>, path: string): T {
-  try {
-    const data = safeLoad(readFileSync(path, 'utf8'));
-    const result = codec.decode(data);
+  const fileData = readFileSync(path, 'utf8');
+  if (!existsSync(path)) {
+    throw new NotFoundError('Cannot find configuration file to load.');
+  }
 
-    if (isRight(result)) {
-      return result.right;
-    } else {
-      throw new ConfigurationError('Configuration schema does not match.');
-    }
-  } catch (error) {
-    throw new ConfigurationError('Cannot load configuration file', error);
+  const data = safeLoad(fileData, { schema: CONFIG_SCHEMA });
+  const result = codec.decode(data);
+
+  if (isRight(result)) {
+    return result.right;
+  } else {
+    throw new ConfigurationError('Configuration schema does not match.');
   }
 }
