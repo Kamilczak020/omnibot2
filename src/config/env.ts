4d2882a90b73487705b14dd1b10d66c9b5fa8fe6
Kamil Solecki <kamilczak020@gmail.com>
2019-12-08 15:21:15 +0100

    Added env type to config parser

---
+++ src/config/env.ts
@@ -0,0 +1,16 @@
+import { Type as YamlType } from 'js-yaml';
+import { NotFoundError } from 'src/error';
+
+export const envType = new YamlType('!env', {
+  kind: 'scalar',
+  resolve(name: string) {
+    if (Reflect.has(process.env, name)) {
+      return true;
+    } else {
+      throw new NotFoundError(`environment variable not found: ${name}`);
+    }
+  },
+  construct(name: string) {
+    return Reflect.get(process.env, name);
+  },
+});
