ef67f2cbc0b7e0ac161e4da32458c0a0c2184136
Kamil Solecki <kamilczak020@gmail.com>
2019-12-11 17:58:27 +0100

    Added getAllByAuthor() method

--- src/repository/message/messageRepository.ts
+++ src/repository/message/messageRepository.ts
@@ -4,6 +4,7 @@ import { injectable, inject } from 'inversify';
 import { Repository as TypeOrmRepository } from 'typeorm';
 import { REPOSITORY_IDENTIFIER } from 'src/constants/repositoryIdentifier';
 import { MessageDataMapper } from './messageDataMapper';
+import { DatabaseError } from 'src/error';
 
 export interface IMessageRepository extends IRepository<MessageDTO> {
 
@@ -18,5 +19,14 @@ export class MessageRepository
     @inject(REPOSITORY_IDENTIFIER.TypeOrmMessageRepository) repository: TypeOrmRepository<MessageEntity>,
   ) {
     super(repository, new MessageDataMapper());
+  }
+
+  public async getAllByAuthor(author: string) {
+    try {
+      const entities = await this._repository.find({ where: { author } });
+      return entities.map((e) => this._dataMapper.toDomain(e));
+    } catch (error) {
+      throw new DatabaseError('Error while getting entities from the database.');
+    }
   }
 }
