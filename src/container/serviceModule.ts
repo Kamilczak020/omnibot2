d54bdfd654d98e3b5d3f79b0661439c1e725897c
Kamil Solecki <kamilczak020@gmail.com>
2019-12-11 17:57:15 +0100

    Bound Matcher service to container

--- src/container/serviceModule.ts
+++ src/container/serviceModule.ts
@@ -3,12 +3,17 @@ import { SERVICE_IDENTIFIER } from 'src/constants';
 import { IBot, Bot } from 'src/core/bot';
 import { ILogger, Logger } from 'src/logger';
 import { Client } from 'discord.js';
+import { IMatcher, Matcher } from 'src/service/matcher';
 
 export const serviceModule = new ContainerModule(
   (bind: interfaces.Bind) => {
     bind<ILogger>(SERVICE_IDENTIFIER.ILogger)
       .to(Logger)
       .inSingletonScope();
+
+    bind<IMatcher>(SERVICE_IDENTIFIER.IMatcher)
+      .to(Matcher)
+      .inRequestScope();
 
     bind<Client>(SERVICE_IDENTIFIER.DiscordClient)
       .toConstantValue(new Client());
