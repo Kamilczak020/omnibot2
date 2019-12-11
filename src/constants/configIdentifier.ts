36c1d4b0895c4e77be22484d708a53267c92886c
Kamil Solecki <kamilczak020@gmail.com>
2019-12-11 17:56:12 +0100

    Added matcherconfig identifier

--- src/constants/configIdentifier.ts
+++ src/constants/configIdentifier.ts
@@ -1,4 +1,5 @@
 export const CONFIG_IDENTIFIER = {
   IConnectionProviderConfig: Symbol.for('IConnectionProviderConfig'),
   IBotConfig: Symbol.for('IBotConfig'),
+  IMatcherConfig: Symbol.for('IMatcherConfig'),
 };
