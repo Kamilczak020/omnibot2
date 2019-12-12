def597d309dedb0db25860e5db14e7d7fc00efdd
Kamil Solecki <kamilczak020@gmail.com>
2019-12-12 17:20:35 +0100

    added echo parser identifier

--- src/constants/configIdentifier.ts
+++ src/constants/configIdentifier.ts
@@ -2,4 +2,5 @@ export const CONFIG_IDENTIFIER = {
   IConnectionProviderConfig: Symbol.for('IConnectionProviderConfig'),
   IBotConfig: Symbol.for('IBotConfig'),
   IMatcherConfig: Symbol.for('IMatcherConfig'),
+  IEchoParserConfig: Symbol.for('IEchoParserConfig'),
 };
