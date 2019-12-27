21d7a085b1fad23817657b5beef99acd1fa5c9f8
Kamilczak020 <kamilczak020@gmail.com>
2019-12-27 02:39:18 +0100

    Registered new repositories

--- src/container/databaseModule.ts
+++ src/container/databaseModule.ts
@@ -3,10 +3,12 @@ import { IConnectionProvider, ConnectionProvider } from 'src/database';
 import { AsyncContainerModule, interfaces } from 'inversify';
 import { Repository as TypeOrmRepository } from 'typeorm';
 import { IMessageRepository, MessageRepository } from 'src/repository/message';
-import { MessageEntity, FilteredMessageEntity } from 'src/entity';
+import { MessageEntity, FilteredEntity, MatchedEntity, ParsedEntity } from 'src/entity';
 import { loadConfig } from 'src/config';
 import { IConnectionProviderConfig, TConnectionProviderConfig } from 'src/config/database';
-import { IFilteredMessageRepository, FilteredMessageRepository } from 'src/repository/filteredMessage';
+import { IFilteredRepository, FilteredRepository } from 'src/repository/filtered';
+import { IMatchedRepository, MatchedRepository } from 'src/repository/matched';
+import { IParsedRepository, ParsedRepository } from 'src/repository/parsed';
 
 export const databaseModule = new AsyncContainerModule(
   async (bind: interfaces.Bind) => {
@@ -20,15 +22,29 @@ export const databaseModule = new AsyncContainerModule(
     bind<TypeOrmRepository<MessageEntity>>(REPOSITORY_IDENTIFIER.TypeOrmMessageRepository)
       .toConstantValue(connection.getRepository(MessageEntity));
 
-    bind<TypeOrmRepository<FilteredMessageEntity>>(REPOSITORY_IDENTIFIER.TypeOrmFilteredMessageRepository)
-      .toConstantValue(connection.getRepository(FilteredMessageEntity));
+    bind<TypeOrmRepository<FilteredEntity>>(REPOSITORY_IDENTIFIER.TypeOrmFilteredRepository)
+      .toConstantValue(connection.getRepository(FilteredEntity));
+
+    bind<TypeOrmRepository<MatchedEntity>>(REPOSITORY_IDENTIFIER.TypeOrmMatchedRepository)
+      .toConstantValue(connection.getRepository(MatchedEntity));
+
+    bind<TypeOrmRepository<ParsedEntity>>(REPOSITORY_IDENTIFIER.TypeOrmParsedRepository)
+    .toConstantValue(connection.getRepository(ParsedEntity));
 
     bind<IMessageRepository>(REPOSITORY_IDENTIFIER.IMessageRepository)
       .to(MessageRepository)
       .inRequestScope();
 
-    bind<IFilteredMessageRepository>(REPOSITORY_IDENTIFIER.IFilteredMessageRepository)
-      .to(FilteredMessageRepository)
+    bind<IFilteredRepository>(REPOSITORY_IDENTIFIER.IFilteredRepository)
+      .to(FilteredRepository)
       .inRequestScope();
+
+    bind<IMatchedRepository>(REPOSITORY_IDENTIFIER.IMatchedRepository)
+    .to(MatchedRepository)
+    .inRequestScope();
+
+    bind<IParsedRepository>(REPOSITORY_IDENTIFIER.IParsedRepository)
+    .to(ParsedRepository)
+    .inRequestScope();
   },
 );
