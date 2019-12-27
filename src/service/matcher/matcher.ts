bc8ae217cb2785cbabfc778f310741d89495f8ed
Kamilczak020 <kamilczak020@gmail.com>
2019-12-27 02:42:09 +0100

    Implemented database persistence through matchedRepository

--- src/service/matcher/matcher.ts
+++ src/service/matcher/matcher.ts
@@ -1,26 +1,30 @@
 import { inject, injectable } from 'inversify';
-import { CONFIG_IDENTIFIER } from 'src/constants';
-import { MessageDTO } from 'src/entity';
+import { CONFIG_IDENTIFIER, REPOSITORY_IDENTIFIER } from 'src/constants';
+import { MessageDTO, MatchedDTO } from 'src/entity';
 import { MatchingError } from 'src/error';
 import { isEmpty } from 'lodash';
 import { IService, BaseService } from 'src/service/base';
-import { IMatcherConfig, IMatchingContext } from 'src/config/service/matcher';
+import { IMatcherConfig } from 'src/config/service/matcher';
+import { IMatchedRepository } from 'src/repository/matched';
 
 export interface IMatcher extends IService {
-  match(message: MessageDTO): [MessageDTO, IMatchingContext];
+  match(message: MessageDTO): Promise<MatchedDTO>;
 }
 
 @injectable()
 export class Matcher extends BaseService implements IMatcher {
   protected config: IMatcherConfig;
+  private readonly matchedRepository: IMatchedRepository;
 
   public constructor(
     @inject(CONFIG_IDENTIFIER.IMatcherConfig) config: IMatcherConfig,
+    @inject(REPOSITORY_IDENTIFIER.IMatchedRepository) repository: IMatchedRepository,
   ) {
     super(config);
+    this.matchedRepository = repository;
   }
 
-  public match(message: MessageDTO): [MessageDTO, IMatchingContext] {
+  public async match(message: MessageDTO): Promise<MatchedDTO> {
     if (isEmpty(message.body)) {
       throw new MatchingError('Cannot match an empty message body.');
     }
@@ -29,11 +33,17 @@ export class Matcher extends BaseService implements IMatcher {
       for (const test of rule.tests) {
         if (test instanceof RegExp) {
           if (this.matchRegexpWithStrategy(message.body, test, rule.strategy)) {
-            return [message, rule.context];
+            const matchedDTO = { id: undefined, parser: rule.context.parser, handler: rule.context.handler, message };
+
+            await this.matchedRepository.insert([matchedDTO]);
+            return matchedDTO;
           }
         } else {
           if (this.matchStringWithStrategy(message.body, test, rule.strategy)) {
-            return [message, rule.context];
+            const matchedDTO = { id: undefined, parser: rule.context.parser, handler: rule.context.handler, message };
+
+            await this.matchedRepository.insert([matchedDTO]);
+            return matchedDTO;
           }
         }
       }
